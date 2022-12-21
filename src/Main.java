a6f523c59a630d9f6fc36882d3ffe14127caa89b
sol namgung <nandaras0103@gmail.com>
2022-12-21 22:14:37 +0900

    1.TwoSum

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        double sum = Math.pow(10,9);
+        System.out.println("heelo 안녕");
     }
 }
