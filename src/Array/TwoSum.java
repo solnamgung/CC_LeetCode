61fca0e56fbe6dad40cbcab5bfe61ce5680224cf
user <nandaras0103@gmail.com>
2023-03-15 17:53:17 +0900

    Array_TwoSum

--- src/Array/TwoSum.java
+++ src/Array/TwoSum.java
@@ -1,23 +1,33 @@
 package Array;
-/*
+import java.util.HashMap;
+
+
+/**
+ * @param {number[]} nums = [2,7,11,15];
+ * @param {number} target = 17;
+ * @return {number[]}
+ */
+
+/**
  * Given an array of integers nums and an integer target, 
  * return indices of the two numbers such that they add up to target.
  * You may assume that each input would have exactly one solution, 
  * and you may not use the same element twice.
- * 
  * You can return the answer in any order.
  */
-public class TwoSum {
 
+
+class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length-1; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    return new int[] {i , j};
-                }
-            }
+        
+    	HashMap<Integer, Integer> tracker = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(tracker.containsKey(nums[i])){
+                int left = tracker.get(nums[i]);
+                return new int[]{left, i};
+            }else
+                tracker.put(target - nums[i], i);
         }
-
-        return new int[] { };
+        return new int[2];
     }
 }
--- src/Array/TwoSum.js
+++ src/Array/TwoSum.js
@@ -1,18 +1,31 @@
 
 /**
  * @param {number[]} nums = [2,7,11,15];
- * @param {number} target = 9;
+ * @param {number} target = 17;
  * @return {number[]}
  */
 
+/**
+ * Given an array of integers nums and an integer target, 
+ * return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, 
+ * and you may not use the same element twice.
+ * You can return the answer in any order.
+ */
+
 	function twoSum(nums, target) {
-	    const map = new Map();
-	    for (let i = 0; i < nums.length; i++) {
-	        const complement = target - nums[i];
-	        if (map.has(complement)) {
-	            return [map.get(complement), i];
-	        }
-	        map.set(nums[i], i);
-	    }
-	    throw new Error("No two sum solution");
+		
+		const map = new Map();
+		
+		for(let i = 0; i < nums.length; i++) {
+			let result = target - nums[i];
+			if(map.has(result)) {
+				return [map.get(result) , i]
+			}
+			
+			map.set(nums[i] , i);
+		}
+		
+		throw new Error ("It's error !");
 	}
+
