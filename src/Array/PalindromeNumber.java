49b3163220588f11721035988da35cff5a4f0cf9
user <nandaras0103@gmail.com>
2023-03-16 17:51:30 +0900

    starting by creating palindromeNumber files

---
+++ src/Array/PalindromeNumber.java
@@ -0,0 +1,10 @@
+package Array;
+/*
+ * Given an integer x, return true if x is a palindrome
+ * ,and false otherwise.
+ * Follow up: Could you solve it without converting the integer to a string?
+ */
+
+public class PalindromeNumber {
+
+}
---
+++ src/Array/PalindromeNumber.js
@@ -0,0 +1,5 @@
+/**
+ * Given an integer x, return true if x is a palindrome
+ * , and false otherwise.
+ * Follow up: Could you solve it without converting the integer to a string?
+ */
